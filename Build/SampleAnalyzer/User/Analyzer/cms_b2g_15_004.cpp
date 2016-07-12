d33b97d6fcbe2fdad9b1a045f94b3f628e51671f
Moltialex <alexis.molter@protonmail.com>
2016-07-12 11:25:38 +0200

    Add Regions and Cuts

--- Build/SampleAnalyzer/User/Analyzer/cms_b2g_15_004.cpp
+++ Build/SampleAnalyzer/User/Analyzer/cms_b2g_15_004.cpp
@@ -8,10 +8,45 @@ using namespace std;
 // -----------------------------------------------------------------------------
 bool cms_b2g_15_004::Initialize(const MA5::Configuration& cfg, const std::map<std::string,std::string>& parameters)
 {
-  cout << "BEGIN Initialization" << endl;
-  // initialize variables, histos
-  cout << "END   Initialization" << endl;
-  return true;
+        // Information on the analysis, authors, ...
+        // VERY IMPORTANT FOR DOCUMENTATION, TRACEABILITY, BUG REPORTS
+        INFO << "        <><><><><><><><><><><><><><><><><><><><><><><><><>" << endmsg;
+        INFO << "        <>  Analysis: CMS-B2G-15-004, arXiv:XXXX.XXXXX  <>" << endmsg;
+        INFO << "        <>            (W' boson, top and bottom quark, lepton final state)       <>" << endmsg;
+        INFO << "        <>  Recasted by: A.MOLTER                       <>" << endmsg;
+        INFO << "        <>  Contact: alexis.molter@protonmail.com       <>" << endmsg;
+        INFO << "        <>  Based on MadAnalysis 5 v1.3                 <>" << endmsg;
+        INFO << "        <>  For more information, see                   <>" << endmsg;
+        INFO << "        <>  http://madanalysis.irmp.ucl.ac.be/wiki/PhysicsAnalysisDatabase" << endmsg;
+
+        Manager()->AddRegionSelection("all");
+        Manager()->AddRegionSelection("1 lepton final state");
+        Manager()->AddRegionSelection("2+ leptons final state");
+        Manager()->AddRegionSelection("electron final state");
+        Manager()->AddRegionSelection("muon final state");
+        Manager()->AddRegionSelection("1 b-tagged");
+        Manager()->AddRegionSelection("2 b-tagged");
+
+        Manager()->AddCut("trigger");
+        Manager()->AddCut("pt lepton> 180 GeV");
+        Manager()->AddCut("delta R (lepton, nearest jet)");
+        Manager()->AddCut("1 lepton final state");
+        Manager()->AddCut("2+ leptons final state");
+
+        string SRForElectron[]= {"1 lepton final state", "2+ leptons final state", "electron final state"};
+        Manager()->AddCut("pt relatif (electron, nearest jet)", SRForElectron);
+        Manager()->AddCut("pt leading jet> 350 GeV", SRForElectron);
+        Manager()->AddCut("pt subleading jet> 30 GeV", SRForElectron);
+        Manager()->AddCut("missing ET> 120 GeV", SRForElectron);
+        Manager()->AddCut("delta phi (missing ET, electron)< 2 rad", SRForElectron);
+
+        string SRForMuon[]= {"1 lepton final state", "2+ leptons final state", "muon final state"};
+        Manager()->AddCut("pt relatif (electron, nearest jet)", SRForMuon);
+        Manager()->AddCut("pt leading jet> 450 GeV", SRForMuon);
+        Manager()->AddCut("pt subleading jet> 30 GeV", SRForMuon);
+        Manager()->AddCut("missing ET> 50 GeV", SRForMuon);
+
+        return true;
 }
 
 // -----------------------------------------------------------------------------
@@ -20,9 +55,9 @@ bool cms_b2g_15_004::Initialize(const MA5::Configuration& cfg, const std::map<st
 // -----------------------------------------------------------------------------
 void cms_b2g_15_004::Finalize(const SampleFormat& summary, const std::vector<SampleFormat>& files)
 {
-  cout << "BEGIN Finalization" << endl;
-  // saving histos
-  cout << "END   Finalization" << endl;
+        cout << "BEGIN Finalization" << endl;
+        // saving histos
+        cout << "END   Finalization" << endl;
 }
 
 // -----------------------------------------------------------------------------
@@ -31,216 +66,6 @@ void cms_b2g_15_004::Finalize(const SampleFormat& summary, const std::vector<Sam
 // -----------------------------------------------------------------------------
 bool cms_b2g_15_004::Execute(SampleFormat& sample, const EventFormat& event)
 {
-  // ***************************************************************************
-  // Example of analysis with generated particles
-  // Concerned samples : LHE/STDHEP/HEPMC
-  // ***************************************************************************
-  /*
-  if (event.mc()!=0)
-  {
-    cout << "---------------NEW EVENT-------------------" << endl;
-
-    // Initial state
-    for (unsigned int i=0;i<event.mc()->particles().size();i++)
-    {
-      const MCParticleFormat& part = event.mc()->particles()[i];
-
-      cout << "----------------------------------" << endl;
-      cout << "MC particle" << endl;
-      cout << "----------------------------------" << endl;
-
-      // display index particle
-      cout << "index=" << i+1;
-
-      // display the status code
-      cout << "Status Code=" << part.statuscode() << endl;
-      if (PHYSICS->Id->IsInitialState(part)) cout << " (Initial state) ";
-      else if (PHYSICS->Id->IsFinalState(part)) cout << " (Final state) ";
-      else cout << " (Intermediate state) ";
-      cout << endl;
-
-      // pdgid
-      cout << "pdg id=" << part.pdgid() << endl;
-      if (PHYSICS->Id->IsInvisible(part)) cout << " (invisible particle) ";
-      else cout << " (visible particle) ";
-      cout << endl;
-
-      // display kinematics information
-      cout << "px=" << part.px()
-                << " py=" << part.py()
-                << " pz=" << part.pz()
-                << " e="  << part.e()
-                << " m="  << part.m() << endl;
-      cout << "pt=" << part.pt() 
-                << " eta=" << part.eta() 
-                << " phi=" << part.phi() << endl;
-
-      // display particle mother id
-      if (part.mother1()==0) 
-      {
-        cout << "particle with no mother." << endl;
-      }
-      else if (part.mother2()==0 || part.mother1()==part.mother2())
-      {
-        const MCParticleFormat* mother = part.mother1();
-        cout << "particle coming from the decay of " 
-             << mother->pdgid() << "." << endl;
-      }
-      else
-      {
-        const MCParticleFormat* mother1 = part.mother1();
-        const MCParticleFormat* mother2 = part.mother2();
-        cout << "particle coming from interaction between "
-             << mother1->pdgid() << " and " << mother2->pdgid()
-             << "." << endl;
-      }
-    }
-
-    // Transverse missing energy (MET)
-    cout << "MET pt=" << event.mc()->MET().pt()
-         << " phi=" << event.mc()->MET().phi() << endl;
-    cout << endl;
-
-    // Transverse missing hadronic energy (MHT)
-    cout << "MHT pt=" << event.mc()->MHT().pt()
-              << " phi=" << event.mc()->MHT().phi() << endl;
-    cout << endl;
-
-    // Total transverse energy (TET)
-    cout << "TET=" << event.mc()->TET() << endl;
-    cout << endl;
-
-    // Total transverse hadronic energy (THT)
-    cout << "THT=" << event.mc()->THT() << endl; 
-   cout << endl;
-
-  return true;
-  }
-  */
-
-
-  // ***************************************************************************
-  // Example of analysis with reconstructed objects
-  // Concerned samples : 
-  //   - LHCO samples
-  //   - LHE/STDHEP/HEPMC samples after applying jet-clustering algorithm
-  // ***************************************************************************
-  /*
-  if (event.rec()!=0)
-  {
-    cout << "---------------NEW EVENT-------------------" << endl;
-
-    // Looking through the reconstructed electron collection
-    for (unsigned int i=0;i<event.rec()->electrons().size();i++)
-    {
-      const RecLeptonFormat& elec = event.rec()->electrons()[i];
-      cout << "----------------------------------" << endl;
-      cout << "Electron" << endl;
-      cout << "----------------------------------" << endl;
-      cout << "index=" << i+1 
-                << " charge=" << elec.charge() << endl;
-      cout << "px=" << elec.px()
-                << " py=" << elec.py()
-                << " pz=" << elec.pz()
-                << " e="  << elec.e()
-                << " m="  << elec.m() << endl;
-      cout << "pt=" << elec.pt() 
-                << " eta=" << elec.eta() 
-                << " phi=" << elec.phi() << endl;
-      cout << "pointer address to the matching MC particle: " 
-                << elec.mc() << endl;
-      cout << endl;
-    }
-
-    // Looking through the reconstructed muon collection
-    for (unsigned int i=0;i<event.rec()->muons().size();i++)
-    {
-      const RecLeptonFormat& mu = event.rec()->muons()[i];
-      cout << "----------------------------------" << endl;
-      cout << "Muon" << endl;
-      cout << "----------------------------------" << endl;
-      cout << "index=" << i+1 
-                << " charge=" << mu.charge() << endl;
-      cout << "px=" << mu.px()
-                << " py=" << mu.py()
-                << " pz=" << mu.pz()
-                << " e="  << mu.e()
-                << " m="  << mu.m() << endl;
-      cout << "pt=" << mu.pt() 
-                << " eta=" << mu.eta() 
-                << " phi=" << mu.phi() << endl;
-      cout << "ET/PT isolation criterion =" << mu.ET_PT_isol() << endl;
-      cout << "pointer address to the matching MC particle: " 
-           << mu.mc() << endl;
-      cout << endl;
-    }
-
-    // Looking through the reconstructed hadronic tau collection
-    for (unsigned int i=0;i<event.rec()->taus().size();i++)
-    {
-      const RecTauFormat& tau = event.rec()->taus()[i];
-      cout << "----------------------------------" << endl;
-      cout << "Tau" << endl;
-      cout << "----------------------------------" << endl;
-      cout << "tau: index=" << i+1 
-                << " charge=" << tau.charge() << endl;
-      cout << "px=" << tau.px()
-                << " py=" << tau.py()
-                << " pz=" << tau.pz()
-                << " e="  << tau.e()
-                << " m="  << tau.m() << endl;
-      cout << "pt=" << tau.pt() 
-                << " eta=" << tau.eta() 
-                << " phi=" << tau.phi() << endl;
-      cout << "pointer address to the matching MC particle: " 
-           << tau.mc() << endl;
-      cout << endl;
-    }
-
-    // Looking through the reconstructed jet collection
-    for (unsigned int i=0;i<event.rec()->jets().size();i++)
-    {
-      const RecJetFormat& jet = event.rec()->jets()[i];
-      cout << "----------------------------------" << endl;
-      cout << "Jet" << endl;
-      cout << "----------------------------------" << endl;
-      cout << "jet: index=" << i+1 
-           << " charge=" << jet.charge() << endl;
-      cout << "px=" << jet.px()
-           << " py=" << jet.py()
-           << " pz=" << jet.pz()
-           << " e="  << jet.e()
-           << " m="  << jet.m() << endl;
-      cout << "pt=" << jet.pt() 
-           << " eta=" << jet.eta() 
-           << " phi=" << jet.phi() << endl;
-      cout << "b-tag=" << jet.btag()
-           << " true b-tag (before eventual efficiency)=" 
-           << jet.true_btag() << endl;
-      cout << "EE/HE=" << jet.EEoverHE()
-           << " ntracks=" << jet.ntracks() << endl;
-      cout << endl;
-    }
-
-    // Transverse missing energy (MET)
-    cout << "MET pt=" << event.rec()->MET().pt()
-         << " phi=" << event.rec()->MET().phi() << endl;
-    cout << endl;
-
-    // Transverse missing hadronic energy (MHT)
-    cout << "MHT pt=" << event.rec()->MHT().pt()
-              << " phi=" << event.rec()->MHT().phi() << endl;
-    cout << endl;
-
-    // Total transverse energy (TET)
-    cout << "TET=" << event.rec()->TET() << endl;
-    cout << endl;
-
-    // Total transverse hadronic energy (THT)
-    cout << "THT=" << event.rec()->THT() << endl;
-    cout << endl;
-  }
-  */
-  return true;
+        return true;
 }
 
